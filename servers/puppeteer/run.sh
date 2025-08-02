5091c882a1dffe511fca749c24053971dd175a68
mutsuyuki <mutsuyuki.tanaka@gmail.com>
2025-08-02 19:29:11 +0900

    allow puppeteer to use gui

--- servers/puppeteer/run.sh
+++ servers/puppeteer/run.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 SERVER_NAME=$(basename $(dirname $0))
 IMAGE_FULLNAME="mcp-${SERVER_NAME}:latest"
 HOST_WORKSPACE="${MCP_HOST_WORKSPACE:-$(pwd)/workspace}"
@@ -10,10 +12,23 @@ docker build \
 --tag  ${IMAGE_FULLNAME} \
 .
 
-# run
+# allow display connection for GUI
+xhost +
+
+# run with GUI support
 docker run \
 --rm \
 --interactive \
+--privileged \
+--shm-size="2g" \
 --user="$(id -u):$(id -g)" \
+--env="QT_X11_NO_MITSHM=1" \
+--env="DISPLAY=${DISPLAY}" \
+--env="WAYLAND_DISPLAY=${WAYLAND_DISPLAY}" \
+--env="XDG_RUNTIME_DIR=${XDG_RUNTIME_DIR}" \
+--env="PULSE_SERVER=${PULSE_SERVER}" \
 --mount="type=bind,src=${HOST_WORKSPACE},dst=${CONTAINER_WORKSPACE}" \
-${IMAGE_FULLNAME}
+--mount="type=bind,src=/tmp/.X11-unix,dst=/tmp/.X11-unix,readonly" \
+--mount="type=bind,src=/run/dbus/system_bus_socket,dst=/run/dbus/system_bus_socket" \
+${IMAGE_FULLNAME} \
+${CONTAINER_WORKSPACE}
